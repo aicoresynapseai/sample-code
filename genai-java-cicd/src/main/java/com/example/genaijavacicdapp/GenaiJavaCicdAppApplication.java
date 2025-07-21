286a4bbc2cfbc565b15b2c073714d9e571833a46
aicoresynapseai <aicoresynapseai@gmail.com>
2025-07-21 13:27:07 +0530

    Add/update genai-java-cicd/src/main/java/com/example/genaijavacicdapp/GenaiJavaCicdAppApplication.java

---
+++ genai-java-cicd/src/main/java/com/example/genaijavacicdapp/GenaiJavaCicdAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.genaijavacicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// GenAI analyzes this class to determine it's a Spring Boot application
+@SpringBootApplication
+public class GenaiJavaCicdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiJavaCicdAppApplication.class, args);
+	}
+
+}
